2f83dc0a32b13ed55629e8e68551d803a60d9150
Stephanie Nemeth <traumverloren@gmail.com>
2017-01-18 21:10:24 +0100

    finished styling, add break and session inputs in JS

--- scripts.js
+++ scripts.js
@@ -1,7 +1,31 @@
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
+const buttons = document.querySelectorAll('button');
+const breakButtons = document.querySelectorAll('.break-button');
+const sessionButtons = document.querySelectorAll('.session-button');
+const breakValue = document.querySelector('.break-value');
+const sessionValue = document.querySelector('.session-value');
+const timer = document.querySelector('.timer');
+
+let sessionTime = 25;
+let breakTime = 5;
+
+// set initial break and session values
+breakValue.innerHTML = breakTime;
+sessionValue.innerHTML = sessionTime;
+
+// set pomodoro timer value
+timer.innerHTML = sessionTime;
+
+function updateBreak() {
+  this.value === 'increase' ? breakTime++ : breakTime--;
+  breakValue.innerHTML = breakTime;
 }
+
+function updateSession() {
+  this.value === 'increase' ? sessionTime++ : sessionTime--;
+  sessionValue.innerHTML = sessionTime;
+  timer.innerHTML = sessionTime;
+}
+
+buttons.forEach(button => button.addEventListener('mousedown', (e => e.preventDefault())));
+breakButtons.forEach(button => button.addEventListener('click', updateBreak));
+sessionButtons.forEach(button => button.addEventListener('click', updateSession));
